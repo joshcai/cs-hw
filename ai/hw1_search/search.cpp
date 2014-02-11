6cd5d2b66ca38a26796ee1edf04f1aba4b7d4a1c
Josh Cai <josh.cai@yahoo.com>
2014-02-10 20:58:25 -0600

    completed assignment 1 (finished with simulated annealing and hill climbing search)

--- ai/hw1_search/search.cpp
+++ ai/hw1_search/search.cpp
@@ -12,15 +12,20 @@
 #include <queue>
 #include <stack>
 #include <map>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+// struct that contains the elements in each node
 struct node
 {
 	struct node *parent;
-	string state;
-	int depth, move, cost;
-	double h;
+	string state; // string representing state
+	int depth, move, cost; 	// depth is depth from root, move is which position the tile moved to to get to this position
+							// cost is how far that tile had to move to get to this position
+	double h; // h is the heuristic evaluation function
 };
 
 bool cost;
@@ -34,13 +39,14 @@ class compareNode {
     }
 };
 
+// heuristic evaluation function - number of tiles out of place
 double h(string state)
 {
 	int total = 0;
 	int w = 0;
 	int b = 0;
 	int n = state.length() / 2;
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < n; i++) // counts white and black tiles in the first n characters (n is half of entire string)
 	{
 		if(state[i] == 'B')
 			b++;
@@ -54,6 +60,7 @@ double h(string state)
 
 }
 
+// tests if in goal position
 bool goal_test(string state)
 {
 	int n = state.length() / 2;
@@ -66,29 +73,41 @@ bool goal_test(string state)
 			break;
 		}
 	}
-	return flag && state[n]=='x';
+	// so far has checked if first n char are 'B'
+	return flag && state[n]=='x'; // so return if also the middle char is 'x' (since all other after must be 'W')
 
 }
 
+// prints path recursively
 void print_path(node* current)
 {
+	// if not head, print_path of parent
 	if(current->parent != NULL)
 	{
 		print_path(current->parent);	
-		cout << "Step " << current->depth << ":   move " << current->move << " " << current->state;
+		cout << "Step " << current->depth << ": move " << current->move << " " << current->state;
 		if(cost)
 		{
 			cout << " (c=" << current->cost << ")";
 		}
 		cout << endl;
 	}
-	else
+	else // base case - initial state
 	{
-		cout << "head";
-		cout << "Step 0:   " << current->state << endl;
+		cout << "Step 0: " << current->state << endl;
 	}
 	return;
 
+}
+
+// deallocates memory when not needed anymore
+void dealloc_nodes(vector<node*>& V)
+{
+	for(int i = 0; i < V.size(); i++)
+	{
+		delete V[i];
+	}
+	V.clear();
 }
 
 int main(int argc, char* argv[])
@@ -99,7 +118,10 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
+		srand(time(NULL));
 		int i = 1;
+		int t = 1; // counts iterations for HCS and time for SA
+		double T;
 		int length, strat;
 		cost = false;
 		if(string(argv[i]) == "-cost")
@@ -108,7 +130,6 @@ int main(int argc, char* argv[])
 			cost = true;
 		}
 		string search_strat = string(argv[i++]);
-		cout << search_strat << endl;
 		strat = 0;
 		if(search_strat == "BFS")
 			strat = 1;
@@ -132,93 +153,138 @@ int main(int argc, char* argv[])
 		initial->parent = NULL;
 		initial->depth = 0;
 		initial->move = -1;
-		initial->h = 0;
-		cout << initial->state << endl;
+		initial->h = h(initial->state);
+		initial->cost = 0;
 		
-
+		// priority queue for UCS, GS, A-star, HCS
 		priority_queue<node*, vector<node*>, compareNode> L;
 		L.push(initial);
 
+		// stack for DFS
 		stack<node*> S;
 		S.push(initial);
 
+		// queue for BFS
 		queue<node*> Q;
 		Q.push(initial);
+		
+		// vector for SA
+		vector<node*> V;
+		// list of pointers to be deleted later
+		vector<node*> delete_list;
+		delete_list.push_back(initial);
 
 
 
+		// map keeps track of visited nodes
 		map<string, bool> checked;
 		node* current;
+		if(strat == 7)
+			current = initial;
 
 		while(!L.empty())
 		{
+			if(strat == 7)
+			{
+				// temperature function
+				T = 1000 * pow(.995, t);
+				if(T == 0)
+					break;
+			}
+
+			if(strat == 6 && t > 10000) // if 10000 iterations and still haven't found for HCS
+				break;
+			t++;
+			// gets current node, if SA, already has current node
 			switch(strat)
 			{
-				case 1:
+				case 1: // BFS
 					current = Q.front();
 					Q.pop();
 					break;
-				case 2:
+				case 2: // DFS
 					current = S.top();
 					S.pop();
 					break;
-				case 3:
-				case 4:
-				case 5:
+				case 3: // UCS
+				case 4: // GS
+				case 5: // A-star
 					current = L.top();
 					L.pop();
 					break;
-				case 6:
+				case 6: // SA
 					current = L.top();
-					L = priority_queue<node*, vector<node*>, compareNode>();
+					// stores current in a temp location before nodes in the queue get deallocated
+					node* temp = new node;
+					temp->state = current->state;
+					temp->depth = current->depth;
+					temp->move = current->move;
+					temp->cost = current->cost;
+					temp->h = current->h;
+					L = priority_queue<node*, vector<node*>, compareNode>(); // clears L
+					dealloc_nodes(delete_list);
+					current = temp;
 					break;
 
 			}
 
-			if(checked.count(current->state) == 0)
+			if(checked.count(current->state) == 0 || strat > 5) // if local search, or not expanded already (in non local)
 			{
 				checked[current->state] = true;
-				cout << current->state << "  " << current->depth << " ";
-				// if(current->parent!=NULL)
-				// {
-				// 	cout << " parent " << current->parent->state << " ";
-				// 	cout << &current << "  " << current->parent << " ";
-				// }
+				// printing code for local search algorithms
+				if(strat>5)
+				{
+					if(t == 2)
+					{
+						cout << "Step 0: " << current->state << endl;
+					}
+					else
+					{
+						cout << "Step " << t-2 << ": move " << current->move << " " << current->state;
+						if(cost)
+							cout << " (c=" << current->cost << ")";
+						cout << endl;
+					}
+
+				}
 				if(goal_test(current->state))
 				{
-					print_path(current);
-					cout << "found goal";
+					// if non-local then print path
+					if(strat < 6)
+						print_path(current);
 					break;
 				}
+				// finds where the x is to see where to switch it
 				int k = 0;
 				while(current->state[k]!='x')
 				{
 					k++;
 				}
-				cout << k << endl;
+				// starts generating successor states
 				for(int j = 0; j < length; j++)
 				{
 					if(j == k)
 						continue;
 					string temp = current->state;
 					node* successor = new node;
-					successor->parent = current;
+					if(strat < 6)
+						successor->parent = current;
 					successor->move = j;
 					temp[k] = temp[j];
 					temp[j] = 'x';
 					successor->cost = ((j < k) ? k-j : j-k); // finds absolute value of j-k
 					successor->state = temp;
 					successor->depth = current->depth+1;
-					// cout << temp << "   " << successor->depth << endl;
+					delete_list.push_back(successor);
 					switch(strat)
 					{
-						case 1:
+						case 1: // BFS - use queue
 							Q.push(successor);
 							break;
-						case 2:
+						case 2: // DFS - use stack
 							S.push(successor);
 							break;
-						case 3:  // UCS
+						case 3:  // UCS - if cost is true, use cost, else, use depth
 							if(cost)
 							{
 								successor->h = (double) successor->cost;
@@ -229,10 +295,11 @@ int main(int argc, char* argv[])
 							}
 							break;
 						case 4: // GS
-						case 6:
+						case 6: // HCS
+								// for both, use h function
 							successor->h = h(successor->state);
 							break;
-						case 5: // A-star
+						case 5: // A-star, if cost is true, use cost + h, else, use depth + h
 							if(cost)
 							{
 								successor->h = (double) successor->cost + h(successor->state);
@@ -242,6 +309,9 @@ int main(int argc, char* argv[])
 								successor->h = (double) successor->depth + h(successor->state);
 							}
 							break;
+						case 7: // SA - use h function
+							successor->h = h(successor->state);
+							V.push_back(successor);
 					}
 					if(strat>2 && strat<7)
 					{
@@ -249,17 +319,55 @@ int main(int argc, char* argv[])
 					}
 
 				}
+				if(strat == 6) // HCS
+				{
+					if((L.top())->h > current->h) // if the smallest of the sucessors is still greater than current, we are stuck
+					{
+						break;
+					}
+				}
+				if(strat == 7) //SA
+				{
+					// choosing random successor
+					int r = rand() % V.size();
+					node* next;
+					next = V[r];
+
+					// can clear vector now
+					V.clear();
+					double change;
+					change = next->h - current->h;
+					if(change < 0) // got better since smaller h is better (tiles out of place)
+					{
+						current = next;
+					}
+					else // got worse
+					{
+						// get random value from 0 to 1
+						double chance = (double) rand() / RAND_MAX;
+						// if less than e^(change/T), next as current
+						if(chance < exp(change/T))
+							current = next;
+					}
+					// placing contents into temp location, since about to deallocate all nodes in delete_list
+					node* temp = new node;
+					temp->state = current->state;
+					temp->depth = current->depth;
+					temp->move = current->move;
+					temp->cost = current->cost;
+					temp->h = current->h;
+					dealloc_nodes(delete_list);
+					current = temp;
+
+				}
 			}
 
 		}
-		cout << "success" << endl;
 
-
+		// free all memory
+		dealloc_nodes(delete_list);
 
 	}
-
-
-	//remember to deallocate memory
 
 
 
